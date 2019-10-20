6f36c1f72b3a5ca2c385980092196d1364454d28
guzheng <wxgz3862@outlook.com>
2019-10-20 17:58:28 +0800

    wb

--- scripts/train_wb.sh
+++ scripts/train_wb.sh
@@ -1,1 +1,1 @@
-CUDA_VISIABLE_DEVICES=0 python train.py --dataroot ./datasets/WebCaricature --name wb_p2c --model cycle_gan --pool_size 50
+python /workspace/cyclegan/train.py --dataroot ./wb_p2c  --checkpoints_dir ./checkpoints --name wb_p2c --model cycle_gan --pool_size 50
